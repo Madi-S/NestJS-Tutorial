c3c7864dac0b6379e41a33c2ef7880eda4444785
Madi-S <shaiken.m@mail.ru>
2022-06-02 17:45:49 +0600

    Local configuration via .env implemented

--- top-api/src/top-page/top-page.controller.ts
+++ top-api/src/top-page/top-page.controller.ts
@@ -8,11 +8,16 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configServece: ConfigService) {
+    this.configServece.get('TEST');
+  }
+
   @Post('create') // by default 201
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
