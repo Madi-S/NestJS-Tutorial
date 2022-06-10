a6e78442f7f51801940e4d856bc78b92d4c68c6b
Madi-S <shaiken.m@mail.ru>
2022-06-10 11:42:22 +0600

    JWT access_token implemented

--- top-api/src/auth/auth.constants.ts
+++ top-api/src/auth/auth.constants.ts
@@ -1,2 +1,4 @@
 export const ALREADY_REGISTERED_ERROR =
   'User with such email already registered';
+export const USER_NOT_FOUND = 'User with such email not found';
+export const WRONG_PASSWORD = 'Incorrect password or email';
