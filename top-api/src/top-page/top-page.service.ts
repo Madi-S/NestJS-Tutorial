570c3f3058935cbed19cc00b3a28f7bb69b01cd1
Madi-S <shaiken.m@mail.ru>
2022-06-18 16:15:27 +0600

    Top page service's findTopLevelCategory method enhanced with aggregation

--- top-api/src/top-page/top-page.service.ts
+++ top-api/src/top-page/top-page.service.ts
@@ -31,7 +31,12 @@ export class TopPageService {
 
   async findByTopLevelCategory(firstCategory: TopLevelCategory) {
     return this.topPageModel
-      .find({ firstCategory }, { alias: 1, secondCategory: 1, title: 1 })
+      .aggregate()
+      .match({ firstCategory })
+      .group({
+        _id: { secondCategory: '$secondCategory' },
+        pages: { $push: { alias: '$alias', title: '$title' } },
+      })
       .exec();
   }
 
