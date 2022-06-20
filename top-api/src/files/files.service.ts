154e7c8d61f9c2a54e03dd34e54db472f001ede0
Madi-S <shaiken.m@mail.ru>
2022-06-20 18:06:39 +0600

    Files endpoint implemented

---
+++ top-api/src/files/files.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { FileElementResponse } from './dto/file-element.response';
+import { format } from 'date-fns';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+
+@Injectable()
+export class FilesService {
+  async saveFiles(
+    files: Express.Multer.File[],
+  ): Promise<FileElementResponse[]> {
+    const dateFolder = format(new Date(), 'yyyy-MM-dd');
+    const uploadFolder = `${path}/uploads/${dateFolder}`;
+    await ensureDir(uploadFolder);
+
+    const response: FileElementResponse[] = [];
+
+    for (const file of files) {
+      const filename = `${uploadFolder}/${file.originalname}`;
+      await writeFile(filename, file.buffer);
+      response.push({
+        url: `${dateFolder}/${file.originalname}`,
+        name: file.originalname,
+      });
+    }
+
+    return response;
+  }
+}
