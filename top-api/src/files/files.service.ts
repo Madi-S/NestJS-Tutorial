a631ee0822433b678cad0ce56f4f2318dc527948
Madi-S <shaiken.m@mail.ru>
2022-06-21 15:48:55 +0600

    Image conversion to webp for file upoad implemented

--- top-api/src/files/files.service.ts
+++ top-api/src/files/files.service.ts
@@ -3,11 +3,13 @@ import { FileElementResponse } from './dto/file-element.response';
 import { format } from 'date-fns';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
+import * as sharp from 'sharp';
+import { MFile } from './mfile.class';
 
 @Injectable()
 export class FilesService {
   async saveFiles(
-    files: Express.Multer.File[],
+    files: MFile[],
   ): Promise<FileElementResponse[]> {
     const dateFolder = format(new Date(), 'yyyy-MM-dd');
     const uploadFolder = `${path}/uploads/${dateFolder}`;
@@ -25,5 +27,9 @@ export class FilesService {
     }
 
     return response;
+  }
+
+  async convertToWebp(file: Buffer): Promise<Buffer> {
+    return sharp(file).webp().toBuffer();
   }
 }
